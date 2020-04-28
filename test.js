2e285291a66e29e7b328e863bc15a82573c7fce1
= <yyb2535@gmail.com>
2020-04-28 18:47:45 +0900

    원시 값과 객체의 비교

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
-{}
+// var count = 10;
+// var price = 1000;
+// var result = 0;
+
+// for (let i = 0; i < count; i++) {
+//   result += price;
+// }
+// if(count >= 10)
+// {
+//   result *= 0.9;
+// }
+// console.log(result);
+
+// const obj = 
+// {
+//   name: 'Lee',
+//   sayHi: function()
+//   {
+//     console.log("?");
+//   },
+//   getHi()
+//   {
+//     console.log("!");
+//   }
+// }
+// obj.sayHi();
+// obj.getHi();
