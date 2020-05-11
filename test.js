6543fd78f759cc60d72671c386023e3f10aeeb7b
= <yyb2535@gmail.com>
2020-05-11 20:27:48 +0900

    모달 및 스크롤

--- test.js
+++ test.js
@@ -24,3 +24,22 @@
 //   }
 // }
 // obj.sayHi();;
+
+function binarySearch (array, target) {
+    let start = 0;
+    let end = array.length - 1;
+    while (start <= end) {
+      const mid = Math.floor((start + end) * 0.5);
+      if (array[mid] === target) return mid;
+      else if (array[mid] > target) end = mid - 1;
+      else start = mid + 1;
+    }
+    return -1;
+  }
+  console.log(binarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
+  console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+  console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+  console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+  console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
+  console.log(binarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
+  console.log(binarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
