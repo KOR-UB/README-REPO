12083cb607e8774bc217de774fd3957d78af8e1c
= <yyb2535@gmail.com>
2020-05-20 22:00:35 +0900

    push

--- test.js
+++ test.js
@@ -25,21 +25,55 @@
 // }
 // obj.sayHi();;
 
-function binarySearch (array, target) {
-    let start = 0;
-    let end = array.length - 1;
-    while (start <= end) {
-      const mid = Math.floor((start + end) * 0.5);
-      if (array[mid] === target) return mid;
-      else if (array[mid] > target) end = mid - 1;
-      else start = mid + 1;
-    }
-    return -1;
-  }
-  console.log(binarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
-  console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
-  console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
-  console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
-  console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
-  console.log(binarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
-  console.log(binarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
+// function binarySearch (array, target) {
+//     let start = 0;
+//     let end = array.length - 1;
+//     while (start <= end) {
+//       const mid = Math.floor((start + end) * 0.5);
+//       if (array[mid] === target) return mid;
+//       else if (array[mid] > target) end = mid - 1;
+//       else start = mid + 1;
+//     }
+//     return -1;
+//   }
+//   console.log(binarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
+//   console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+//   console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+//   console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+//   console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
+//   console.log(binarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
+//   console.log(binarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
+
+// (function printNow() {
+//   const today = new Date();
+
+//   const dayNames = ['(일요일)', '(월요일)', '(화요일)', '(수요일)', '(목요일)', '(금요일)', '(토요일)'];
+//   // getDay: 해당 요일(0 ~ 6)를 나타내는 정수를 반환한다.
+//   const day = dayNames[today.getDay()];
+
+//   const year = today.getFullYear();
+//   const month = today.getMonth() + 1;
+//   const date = today.getDate();
+//   let hour = today.getHours();
+//   let minute = today.getMinutes();
+//   let second = today.getSeconds();
+//   const ampm = hour >= 12 ? 'PM' : 'AM';
+
+//   // 12시간제로 변경
+//   hour %= 12;
+//   hour = hour || 12; // 0 => 12
+
+//   // 10미만인 분과 초를 2자리로 변경
+//   minute = minute < 10 ? '0' + minute : minute;
+//   second = second < 10 ? '0' + second : second;
+
+//   const now = `${year}년 ${month}월 ${date}일 ${day} ${hour}:${minute}:${second} ${ampm}`;
+
+//   console.log(now);
+//   setTimeout(printNow, 1000);
+// }());
+
+const values = [1, 2, 1, 3, 5, 4, 5, 3, 4, 4];
+
+const result = values.filter((v, i, arr) => arr.indexOf(v) === i);
+console.log(result);
---
+++ test2.js
@@ -0,0 +1,90 @@
+// const todos = [{
+//   id: 3,
+//   content: 'HTML',
+//   completed: false
+// },
+// {
+//   id: 2,
+//   content: 'CSS',
+//   completed: true
+// },
+// {
+//   id: 1,
+//   content: 'Javascript',
+//   completed: false
+// }
+// ];
+
+// function sortBy(key) {
+//  return todos.slice().sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
+// }
+
+// console.log(sortBy('id'));
+// /*
+// [
+//   { id: 1, content: 'Javascript', completed: false },
+//   { id: 2, content: 'CSS', completed: true },
+//   { id: 3, content: 'HTML', completed: false }
+// ]
+// */
+// console.log(sortBy('content'));
+// /*
+// [
+//   { id: 2, content: 'CSS', completed: true },
+//   { id: 3, content: 'HTML', completed: false },
+//   { id: 1, content: 'Javascript', completed: false }
+// ]
+// */
+// console.log(sortBy('completed'));
+// /*
+// [
+//   { id: 3, content: 'HTML', completed: false },
+//   { id: 1, content: 'Javascript', completed: false },
+//   { id: 2, content: 'CSS', completed: true }
+// ]
+// */
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+function addTodo(newTodo) {
+  todos = [newTodo, ...todos];
+}
+
+addTodo({ id: 4, content: 'Test', completed: false });
+
+console.log(todos);
+
+// function removeTodo(id) {
+//   todos.splice(id - 1, 1);
+// }
+
+// removeTodo(2);
+
+// console.log(todos);
+/*
+[
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+function countCompletedTodos() {
+  // let num = 0;
+  // todos.filter(todo => todo.completed === true ? ++num : num);
+  return todos.filter(todo => todo.completed === true).length;
+  // return num;
+}
+
+console.log(countCompletedTodos()); // 1
+
+function getMaxId() {
+//   // let max = 0;
+//   todos.filter(todo => max = todo.id > max ? todo.id : max);
+//   // return max;
+  // let max = 0;
+  // return todos.filter(todo => todo.id > max).length;
+  return Math.max(...todos.map(todo => todo.id));
+}
+console.log(getMaxId()); // 3
